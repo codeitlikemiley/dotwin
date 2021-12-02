26900ba2d32021f9df3e57c5b65103349247f828
GOLDCODERS <ceo@goldcoders.dev>
2021-12-02 22:58:05 +0800

    update powershell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,7 +10,7 @@ Import-Module npm-completion
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PsFzfOption -TabExpansion
 # Use VI MODE
-Set-PSReadLineOption -editmode vi
+Set-PSReadlineOption -EditMode vi -BellStyle None
 # Use Tab Completion
 Set-PSReadLineKeyHandler -chord = -function Complete -vimode Command
 
@@ -19,36 +19,34 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory
 # Use history for Autocompetion
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-    $Local:word = $wordToComplete.Replace('"', '""')
-    $Local:ast = $commandAst.ToString().Replace('"', '""')
-    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
-}
+
+# Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+#     param($wordToComplete, $commandAst, $cursorPosition)
+#     [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+#     $Local:word = $wordToComplete.Replace('"', '""')
+#     $Local:ast = $commandAst.ToString().Replace('"', '""')
+#     winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+#     [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+#     }
+# }
 
 # -------------------------------------------------------------------------------------------------
 # Remove Aliases
 # Windows Built in Aliases , We need to Use Unix Counter part
-Remove-Item alias:cat
-Remove-Item alias:cp
-Remove-Item alias:diff -Force
-Remove-Item alias:echo
-Remove-Item alias:rm
-Remove-Item alias:ls
 Remove-Item alias:gl -Force
+Remove-Item alias:rp -Force
 
 # -------------------------------------------------------------------------------------------------
 # Fuzzy Finder Commands
-function Ccd () {
-	Set-Location (Set-Location (Get-ChildItem . -Recurse | Where-Object { $_.PSIsContainer } | Invoke-Fzf))
+# function Ccd () {
+# 	Set-Location (Set-Location (Get-ChildItem . -Recurse | Where-Object { $_.PSIsContainer } | Invoke-Fzf))
+# }
+if ($PSVersionTable.PSVersion.Major -gt 5){
+function ll { Get-ChildItem -Path (Get-Location) | Format-Table -View childrenWithHardLink }
 }
+function cwd(){Convert-Path . | clip.exe}
 
-function cwd(){Get-Location | tail -n 2 | clip.exe}
-
-function p{fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'}
+#function p{fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'}
 # Start Port Killer
 function Find-Port(){
 	netstat -ano | findstr :$args
@@ -62,8 +60,13 @@ function Invoke-Kill(){
 function rf($filepath) { Remove-Item -Path $filepath -Recurse -Force -ErrorAction SilentlyContinue -Confirm }
 function l { Get-ChildItem -Path (Get-Location) }
 function c {Clear-Host}
+
+function v {nvim $args}
+
+function vim{nvim}
 function ls { Get-ChildItem -Path (Get-Location) -ReadOnly}
-function ll { Get-ChildItem -Path (Get-Location) | Format-Table -View childrenWithHardLink }
+## define all functions less than 6
+
 function la { Get-ChildItem -Path (Get-Location) -Name}
 function lc {Get-ChildItem -Path Cert:\* -Recurse -CodeSigningCert}
 function pkill($name) { get-process $name -ErrorAction SilentlyContinue | stop-process }
@@ -92,6 +95,7 @@ function touch($file) {
 function New-Link($target, $link) {
 	New-Item -ItemType SymbolicLink -Path $link -Value $target
 }
+
 # http://stackoverflow.com/questions/39148304/fuser-equivalent-in-powershell/39148540#39148540
 function fuser($relativeFile) {
 	$file = Resolve-Path $relativeFile
@@ -130,7 +134,7 @@ function reboot {
 	shutdown /r /t 0
 }
 # Windows Path Shortcut CMD
-function New-Path {
+function Add-Path {
 	param(
 		[string]$Dir
 	)
@@ -170,7 +174,7 @@ function glf { git log --graph --oneline --name-only }
 # Show full commits
 function glF { git log -p }
 # Remove File From Git Versioning but file still available Locally
-function grf { git rm --cached $args }
+function grm { git rm --cached $args }
 # Remove All Changes
 function nah { git reset --hard; git clean -df }
 function Add-Tag { git tag -a -f $args }
@@ -180,31 +184,36 @@ function Push-Tag { git push origin --tags --force }
 function Add-Wip { "git add . && git commit -m 'wip'" }
 # -------------------------------------------------------------------------------------------------
 # Laravel Commands
-function Invoke-Tinker-Command { php artisan tinker }
-function Invoke-Artisan-Command { php artisan $args }
-function Invoke-Fresh-Command { php artisan migrate:fresh --seed }
-function Invoke-Phpunit { .\vendor\bin\phpunit $args }
+#function Invoke-Tinker-Command { php artisan tinker }
+#function Invoke-Artisan-Command { php artisan $args }
+#function Invoke-Fresh-Command { php artisan migrate:fresh --seed }
+#function Invoke-Phpunit { .\vendor\bin\phpunit $args }
 function Update-FlutterSDK { pub global run fvm:main use $args}
 function Get-Netlify-Config { code $env:appdata\netlify\Config\config.json}
 function Get-Profiles {$PROFILE | Select-Object *Host* | Format-List}
+
+function Invoke-Reload-Profile {
+	.$Profile
+}
 # -------------------------------------------------------------------------------------------------
 # Add All Alias to Commands Shortcuts
 set-alias ln New-Link
+set-alias rp Invoke-Reload-Profile
 # set-alias -Name j -Value jrnl.exe -Option AllScope
 set-alias -Name tag -Value Add-Tag -Option AllScope
 set-alias -Name ptag -Value Push-Tag -Option AllScope
-Set-Alias -Name fresh -Value Invoke-Fresh-Command -Option AllScope
+#Set-Alias -Name fresh -Value Invoke-Fresh-Command -Option AllScope
 Set-Alias -Name wip -Value Add-Wip -Option AllScope
-Set-Alias -Name tinker -Value Invoke-Tinker-Command -Option AllScope
-Set-Alias -Name sdk -Value Update-FlutterSDK -Option AllScope
-Set-Alias -Name t -Value Invoke-Phpunit -Option AllScope
-Set-Alias -Name f -Value flutter.exe -Option AllScope
-Set-Alias -Name d -Value dart.exe -Option AllScope
+#Set-Alias -Name tinker -Value Invoke-Tinker-Command -Option AllScope
+#Set-Alias -Name sdk -Value Update-FlutterSDK -Option AllScope
+#Set-Alias -Name t -Value Invoke-Phpunit -Option AllScope
+#Set-Alias -Name f -Value flutter.exe -Option AllScope
+#Set-Alias -Name d -Value dart.exe -Option AllScope
 # set-alias -Name vim -Value nvim -Option AllScope
 # -------------------------------------------------------------------------------------------------
 # Directories Functions that will be Alias
-function Set-Path-www { Set-Location -Path $env:USERPROFILE\www }
-function Set-Path-App { Set-Location -Path $env:USERPROFILE\App }
+#function Set-Path-www { Set-Location -Path $env:USERPROFILE\www }
+#function Set-Path-App { Set-Location -Path $env:USERPROFILE\App }
 function Set-Path-Home { Set-Location -Path $env:USERPROFILE }
 function Set-Path-Win32 { Set-Location -Path $env:windir\System32 }
 function Get-Back-OneDIR { Set-Location -Path ../ }
@@ -213,9 +222,9 @@ function Get-Back-ThreeDIR { Set-Location -Path ../../../ }
 function Get-Back-FourDIR { Set-Location -Path ../../../../ }
 function Set-Path-Workspace { Set-Location -Path $env:USERPROFILE\.workspace}
 # Directory Aliases
-Set-Alias -Name www -Value Set-Path-www -Option AllScope
-Set-Alias -Name wp -Value Set-Path-Workspace -Option AllScope
-Set-Alias -Name app -Value Set-Path-App -Option AllScope
+#Set-Alias -Name www -Value Set-Path-www -Option AllScope
+Set-Alias -Name sws -Value Set-Path-Workspace -Option AllScope
+#Set-Alias -Name app -Value Set-Path-App -Option AllScope
 Set-Alias -Name h -Value Set-Path-Home -Option AllScope
 Set-Alias -Name '..' -Value Get-Back-OneDIR -Option AllScope
 Set-Alias -Name '..2' -Value Get-Back-TwoDIR -Option AllScope
@@ -232,36 +241,42 @@ function Open-VSCode-Keys { code $env:appdata\Code\User\keybindings.json }
 function Open-VSCode-Settings { code $env:appdata\Code\User\settings.json }
 # Other Config
 function Open-Vim-Config { code $env:LOCALAPPDATA\nvim\init.vim }
-function Open-Alacritty-Config { code $env:appdata\alacritty\alacritty.yml }
+#function Open-Alacritty-Config { code $env:appdata\alacritty\alacritty.yml }
 function Open-Etc-Host { code $env:windir\System32\Drivers\etc\hosts }
 function Open-Profile { code $profile}
-function Export-Firestore { gcloud beta firestore export --collection-ids=$args}
+#function Export-Firestore { gcloud beta firestore export --collection-ids=$args}
+
 
 # Config Aliases
 Set-Alias -Name cfv -Value Open-Vim-Config -Option AllScope
+Set-Alias -Name cfV -Value Open-VSCode-Settings -Option AllScope
+Set-Alias -Name cfk -Value Open-VSCode-Keys -Option AllScope
 Set-Alias -Name etc -Value Open-Etc-Hosts -Option AllScope
 Set-Alias -Name pro -Value Open-Profile -Option AllScope
 Set-Alias -Name nc -Value Get-Netlify-Config -Option AllScope
 Set-Alias -Name cfa -Value Open-Alacritty-Config -Option AllScope
 Set-Alias -Name e -Value explorer.exe -Option AllScope
+Set-Alias -Name ws -Value Open-VSCode-Workspace -Option AllScope
 
 # Fuzzy finder alias
-Set-Alias -Name fkill -Value Invoke-FuzzyKillProcess -Option AllScope
+#Set-Alias -Name fkill -Value Invoke-FuzzyKillProcess -Option AllScope
 # Install-Module ZLocation -Scope CurrentUser
-Set-Alias -Name fz -Value Invoke-FuzzyZLocation -Option AllScope
-Set-Alias -Name fgs -Value Invoke-FuzzyGitStatus -Option AllScope
-Set-Alias -Name fh -Value Invoke-FuzzyHistory -Option AllScope
-Set-Alias -Name fd -Value Invoke-FuzzySetLocation -Option AllScope
+#Set-Alias -Name fz -Value Invoke-FuzzyZLocation -Option AllScope
+#Set-Alias -Name fgs -Value Invoke-FuzzyGitStatus -Option AllScope
+#Set-Alias -Name fh -Value Invoke-FuzzyHistory -Option AllScope
+#Set-Alias -Name fd -Value Invoke-FuzzySetLocation -Option AllScope
 
 # -------------------------------------------------------------------------------------------------
 # Beautify our terminal
 Invoke-Expression (&starship init powershell)
 # FNM Node Js
 fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
-#fnm env --use-on-cd | Out-String | Invoke-Expression
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell) -join "`n"
 })
 # Enable MenuComplete on Tab
+# https://docs.microsoft.com/en-us/powershell/module/psreadline/about/about_psreadline?view=powershell-7.2#basic-editing-functions
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Set-PSReadLineKeyHandler -Chord Ctrl+e -ViMode Insert -ScriptBlock {
+# }
